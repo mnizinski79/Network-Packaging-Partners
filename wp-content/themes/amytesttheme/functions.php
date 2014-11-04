bdc3da5f0250b39a2642c3a0883e63fb7151401b
amylashley <amylashley@gmail.com>
2014-11-04 14:03:51 -0500

    updated functions.php

--- wp-content/themes/amytesttheme/functions.php
+++ wp-content/themes/amytesttheme/functions.php
@@ -28,3 +28,11 @@ add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
 
 
 add_theme_support( 'post-thumbnails' );
+
+/*
+* Switch default core markup for search form, comment form, and comments
+* to output valid HTML5.
+*/
+add_theme_support( 'html5', array(
+	'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
+) );
