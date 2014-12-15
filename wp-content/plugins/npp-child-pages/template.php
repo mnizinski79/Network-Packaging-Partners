4e7debbec4da819f94686ae67a7f3693a8e48457
amylashley <amylashley@gmail.com>
2014-12-15 11:49:53 -0500

    fixed conflict
    fixed conflict

--- wp-content/plugins/npp-child-pages/template.php
+++ wp-content/plugins/npp-child-pages/template.php
@@ -1,5 +1,8 @@
+
+
+
 <?php
-$my_content;
+$my_content;$title = ''; $caption = ''; $description = ''; 
 // Set up the objects needed
 $my_wp_query = new WP_Query();
 $all_wp_pages = $my_wp_query->query(array('post_type' => 'page'));
@@ -12,11 +15,26 @@ if ($my_children && sizeof($my_children)>0){
                     <div class="col-container">';
 
   foreach ($my_children as $child) {
+     if ( has_post_thumbnail( $child->ID ) ) :
+      $image_array = wp_get_attachment_image_src( get_post_thumbnail_id( $child->ID ), 'full' );
+      $image = $image_array[0];
+      $title = get_post(get_post_thumbnail_id($child->ID))->post_title; //The Title
+      $caption = get_post(get_post_thumbnail_id($child->ID))->post_excerpt; //The Caption
+      $description = get_post(get_post_thumbnail_id($child->ID))->post_content; // The Descriptio
+    else :
+        $image = plugins_url( "img/svgs/img-ico-design.svg", __FILE__ );
+        $title = $child->post_title; 
+        $caption = substr($child->post_content, 0, 300); //The Caption
+      endif;
+    
+    if ($caption == ''){
+      $caption = substr($child->post_content, 0,300);
+    }
 
-    $my_content .= '<a href="'.$child->guid.'" class="col-4 page-lead" data-title="'.$child->post_title.'" data-content="'.substr($child->post_content, 0,200).'">
-                            <figure><img src="'.plugins_url( "img/svgs/img-ico-design.svg", __FILE__ ).'"></figure>
+    $my_content .= '<a href="'.$child->guid.'" class="col-4 page-lead" data-title="'.$title.'" data-content="'.$caption.'">
+                            <figure><img src="'.$image.'"></figure>
                             <h3>'.$child->post_title.'</h3>
-                            <p>'.date('M. d, Y',strtotime($child->post_date)).'</p>
+                            <p>'.$description.'</p>
                         </a>';
   }
 
